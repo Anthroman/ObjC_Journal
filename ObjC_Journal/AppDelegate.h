6066069b4a7cfe33b45270eb6d79e39246e1393e
Alex Lundquist <alex@alexludnquist.com>
2019-03-25 13:41:06 -0600

    Initial Commit

---
+++ ObjC_Journal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjC_Journal
+//
+//  Created by Alex Lundquist on 3/25/19.
+//  Copyright © 2019 Alex Lundquist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjC_Journal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjC_Journal
+//
+//  Created by Alex Lundquist on 3/25/19.
+//  Copyright © 2019 Alex Lundquist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
